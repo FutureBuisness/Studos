50c043ecffc47bf6dbb70c130c70d013e6d60141
FutureBuisness <future-business@outlook.com>
2019-12-11 20:31:33 +0100

    Added linter n circleci config.

--- src/main/java/Studos/App.java
+++ src/main/java/Studos/App.java
@@ -1,13 +1,27 @@
-package Studos;
+/**
+ * Info about this package doing something for package-info.java file.
+ */
+
+package studos;
 
 /**
- * Hello world!
- *
+ * Simple JavaDoc.
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+public final class App {
+    /**
+     * Simple Constructor JavaDoc.
+     */
+    private App() {
+        // not called
+    }
+
+    /**
+     * Simple JavaDoc.
+     * This shit is horrible af.
+     * @param args takes args to the to the main.
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello World!");
     }
 }
