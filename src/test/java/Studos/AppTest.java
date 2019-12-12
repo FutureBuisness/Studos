fbd9610927eafb81fdbc21c854ab709b053defcf
FutureBuisness <future-business@outlook.com>
2019-12-12 22:31:47 +0100

    Fixed AppTest

--- src/test/java/Studos/AppTest.java
+++ src/test/java/Studos/AppTest.java
@@ -1,4 +1,4 @@
-package Studos;
+package studos;
 
 import static org.junit.Assert.assertTrue;
 
@@ -7,14 +7,12 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
-     * Rigorous Test :-)
+     * Rigorous Test :-).
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
     }
 }
