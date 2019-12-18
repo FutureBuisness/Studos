fb7e1654af499c95012c8c756849def2dceaa4b9
Oleg <creduntvitam@gmail.com>
2019-12-18 01:09:40 +0100

    Removed Main from App.java

--- src/main/java/studos/logic/App.java
+++ src/main/java/studos/logic/App.java
@@ -3,23 +3,3 @@
  */
 
 package studos.logic;
-
-/**
- * Simple JavaDoc.
- */
-
-public final class App {
-    /**
-     * Simple Constructor JavaDoc.
-     */
-    private App() {
-    }
-
-    /**
-     * Simple JavaDoc.
-     * @param args takes args.
-     */
-    public static void main(final String[] args) {
-        System.out.println("Hello World!");
-    }
-}
